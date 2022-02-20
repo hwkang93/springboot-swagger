2510fda1973ca74307be91efa1071375937ca1a3
hwkang <landman93@naver.com>
2022-02-20 17:38:46 +0900

    주석 추가

--- src/main/java/hwkang/springbootswagger/user/service/UserService.java
+++ src/main/java/hwkang/springbootswagger/user/service/UserService.java
@@ -6,11 +6,29 @@ import java.util.List;
 
 public interface UserService {
 
+    /**
+     * 사용자 전체 목록 조회
+     * @return List<UserDto>
+     */
     List<UserDto> findAll();
 
+    /**
+     * 사용자 ID 로 사용자 조회
+     * @param userId
+     * @return UserDto
+     */
     UserDto findByUserId(long userId);
 
+    /**
+     * 사용자 추가
+     * @param userDto
+     * @return userDto
+     */
     UserDto insertUser(UserDto userDto);
 
+    /**
+     * 사용자 삭제
+     * @param userId
+     */
     void deleteUser(long userId);
 }
